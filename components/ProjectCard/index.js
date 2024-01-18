e346cbd79974b35625c8bcd09c9a5bd7cceff566
山杉 <dramalfcsc@gmail.com>
2024-01-18 20:15:02 +0800

    feat:避免重复拉取web component html

--- components/ProjectCard/index.js
+++ components/ProjectCard/index.js
@@ -1,40 +1,48 @@
 // import html from './card.html';
 // console.log(html)
 class ProjectCard extends HTMLElement {
-    static observedAttributes = ['lang','index'];
+    static observedAttributes = ['lang', 'index'];
     constructor() {
         // Always call super first in constructor
         super();
     }
-    
+
     connectedCallback() {
-        fetch('./components/ProjectCard/index.html').then(res => res.text()).then(html => {
-            this.originHtml=html;
-            const index=this.getAttribute('index');
-            const lang=this.getAttribute('lang');
-            const coverImg=`./assets/p${index}.jpg`;
-            const name=window.languageObj[lang][`project${index}name`];
-            const introduction=window.languageObj[lang][`project${index}introduction`];
-            const state={
-                coverImg,name,introduction
-            };
-            ['coverImg','name','introduction'].forEach(attr => {
-                const v = state[attr];
-                if (v) {
-                    const reg = new RegExp(`{{${attr}}}`, 'g')
-                    html = html.replace(reg,v)
-                }
-            })
-            this.innerHTML = html
-        })
+        this.render()
     }
-    attributeChangedCallback(attr,oldValue,newValue){
-        if(attr==='lang'&&this.innerHTML){
-            const index=this.getAttribute('index');
-            let nameDom=this.querySelector('.project-name');
-            nameDom.innerHTML=window.languageObj[newValue][`project${index}name`];
-            let introDom=this.querySelector('.project-intro');
-            introDom.innerHTML=window.languageObj[newValue][`project${index}introduction`];
+    async render() {
+        let html;
+        if (!ProjectCard.originHTML) {
+            ProjectCard.originHTML = fetch('./components/ProjectCard/index.html').then(res => res.text());
+            html = await ProjectCard.originHTML;
+            ProjectCard.originHTML = html;
+        } else {
+            html = await Promise.resolve(ProjectCard.originHTML);
+        }
+        const index = this.getAttribute('index');
+        const lang = this.getAttribute('lang');
+        const coverImg = `./assets/p${index}.jpg`;
+        const name = window.languageObj[lang][`project${index}name`];
+        const introduction = window.languageObj[lang][`project${index}introduction`];
+        const state = {
+            coverImg, name, introduction
+        };
+        ['coverImg', 'name', 'introduction'].forEach(attr => {
+            const v = state[attr];
+            if (v) {
+                const reg = new RegExp(`{{${attr}}}`, 'g')
+                html = html.replace(reg, v)
+            }
+        })
+        this.innerHTML = html
+    }
+    attributeChangedCallback(attr, oldValue, newValue) {
+        if (attr === 'lang' && this.innerHTML) {
+            const index = this.getAttribute('index');
+            let nameDom = this.querySelector('.project-name');
+            nameDom.innerHTML = window.languageObj[newValue][`project${index}name`];
+            let introDom = this.querySelector('.project-intro');
+            introDom.innerHTML = window.languageObj[newValue][`project${index}introduction`];
         }
     }
 }
